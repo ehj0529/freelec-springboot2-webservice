208bfc4d2c55149db53ab7c2b9d24679501c839e
ehj0529 <63188985+ehj0529@users.noreply.github.com>
2020-05-05 22:56:09 +0900

    5.3구글연동하기

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -30,11 +30,15 @@ public class PostsService {
 
     @Transactional
     public Long update( Long id, PostsUpdateRequestDto requestDto ) {
-
         Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
-
         return id;
+    }
+
+    @Transactional
+    public void delete( Long id ) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById( Long id ) {
